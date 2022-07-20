4540cb1ea3003884fca2ead9962731aeea54ce1a
RoufanZ <108813091+RoufanZ@users.noreply.github.com>
2022-07-20 22:01:06 +0800

    Update and rename iOS开发语言入门作业.txt to iOS开发语言入门作业/student.h

--- iOS开发语言入门作业/student.h
+++ iOS开发语言入门作业/student.h
@@ -1,15 +1,7 @@
-//student.h
 @interface Student:NSObject{
     NSInteger age;  //年龄
     NSString name;  //姓名
     NSString major; //专业
 }
 -(return_type) study: (CGFloat) time;   //学习时间
-@end
-
-//student.m
-@implementation Student{}
--(return_type) study: (CGFloat) time {
-    NSLog(@"ld", time);
-}
 @end
