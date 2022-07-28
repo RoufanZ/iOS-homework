6b3d2f307841bf07250c02818284498adc3f7253
RoufanZ <108813091+RoufanZ@users.noreply.github.com>
2022-07-28 13:17:50 +0800

    Create AppDelegate.m

---
+++ College Demo homework/AppDelegate.m
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.m
+//  College Demo
+//
+//  Created by mashujun on 2022/7/18.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+
+#pragma mark - UISceneSession lifecycle
+
+
+- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+}
